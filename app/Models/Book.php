badf8f7b836d46aede8ce1d3f7178713e53c4e77
Asterio <casimirodaniel24@gmail.com>
2026-04-13 17:09:54 +0800

    updated

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    // ADDED: Allow mass assignment for these specific columns
+    protected $fillable = [
+        'title',
+        'category_id',
+    ];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
